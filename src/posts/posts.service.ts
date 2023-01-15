b18c7f308259fbc6cec87b25a0f8549f4b851b0a
exp <arzuakov@yahoo.com>
2023-01-16 02:46:05 +0300

    h16.01.2023

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { FilesService } from 'src/files/files.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Post } from './posts.model';
 
 @Injectable()
 export class PostsService {
-  constructor(@InjectModel(Post) private posrRepository: typeof Post) { }
+  constructor(@InjectModel(Post) private posrRepository: typeof Post,
+  private fileService: FilesService) { }
 
   async create(dto: CreatePostDto, image: any) {
-    const fileName = ''
+    const fileName = await this.fileService.createFile(image)
     const post = this.posrRepository.create({ ...dto, image: fileName })
     return post
   }
